1a26950eb8ab1786ebaca43752b398f0d65846c2
Vincent Lind <vincent.lind@elev.ga.ntig.se>
2025-03-13 14:26:15 +0100

    Tränat på javascript och variabler, konkatenering

---
+++ kapitle-8/intro-js/script.js
@@ -0,0 +1,18 @@
+// var tal1 = 12;
+// var tal2 = 34;
+// var summa = tal1 + tal2
+// alert(tal1 + " + " + tal2 + " = " + summa);
+
+
+var förnamn = "Vince";
+var efternamn = "Blind";
+var ålder = "18";
+var namn = förnamn + " " + efternamn;
+var meddelande = förnamn + " är " + ålder + "år gammal"
+// alert(namn);
+
+var hobby = "tvspel";
+var kostnad = 1000; 
+var meddelande =  namn + " gillar " + hobby + "\noch det kostar " + kostnad + "per ålder." ;
+// Tex Vince Lind gillar tvspel och det kostar el räkningen
+alert();
