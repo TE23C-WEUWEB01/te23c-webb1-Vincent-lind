33fbc7dbf7ef8be5dc522f1665a63c66a007dcb8
Vincent Lind <vincent.lind@elev.ga.ntig.se>
2025-03-21 09:44:18 +0100

    Tränat på if-satster och loopar

---
+++ kapitle-8/grunder-js/script.js
@@ -0,0 +1,33 @@
+// här skriver js-kod
+// var tal1 = parseInt(prompt("skriv in ett heltal:"))
+// var tal2 = parseInt(prompt("skriv in ett till heltal:"))
+
+// if ( tal1 > tal2) {
+//     document.writeln(`${tal1} är större än ${tal2}`)
+// }else {
+//     document.writeln(`${tal2} är större eller lika med ${tal1}`)
+// }
+
+//en liten inloggning, dvs fråga efter koncto & lösenord och jämför med valbart lösenord
+
+//svara att "du ... är inloggad " annars " Du ... har skrivit fel lösenord"
+
+// var namn = parseInt = prompt("Vad heter du?:");
+// var lösenord = prompt("ange din kåd:") 
+
+// if (lösenord == "12345") {
+//     document.writeln(`Du ${namn} är inloggad`);
+//     break;
+// }else {
+//     document.writeln(`Du $(namn) har skrivit fel lösenord`);
+// }
+
+// for (let index = 20; index < 30; index++) {
+//     document.writeln(`tal: ${index} <br>`);
+// }
+
+//let val = confirm("klicka för att forsätta");
+let val = true;
+while (val) {
+    val = confirm("klicka Ok för att forsätta") 
+}
